6984e83d52ac668ecc1430d42b043abf1feb3e44
Grigoryan <KhorenGrig>
2018-03-10 02:03:54 +0400

    Do

--- problems.js
+++ problems.js
@@ -1,15 +1,26 @@
-this.name = 'THIS IS THE GLOBAL OBJECT';
+// this.name = 'THIS IS THE GLOBAL OBJECT';
 
-const obj = {
-  name: 'a',
-  age: 14
-};
+// const obj = {
+//   name: 'a',
+//   age: 14
+// };
+
+
+// const ff = () =>{
+//   console.log(this.name);
+// }
+// ff();
+
+// const ff = function(){
+//   console.log(this.name);
+// }
+
+// const newff = ff.bind(obj);
+// newff();
 
 // make a function that will console log this.name,
 // make it console log obj.name when calling
 // dont use call or apply
-
-
 
 
 
@@ -18,31 +29,85 @@ const obj = {
 // create a new array that will be the reverse of the originalArray
 
 
-const originalArray = [0,1,2,3,4,5,6,7,8,9];
+// const originalArray = [0,1,2,3,4,5,6,7,8,9];
 
-const reversedArray = (arr) => {
-  arr.reverse();
-};
+// const reversedArray = (arr) => {
+//   const newarr = [...arr];
+//   return newarr.reverse();
+// };
 
-reversedArray(originalArray);
-console.log(originalArray);
-
-
-
+// console.log(reversedArray(originalArray));
+// console.log(originalArray);
 
 
 // make this code shorter
 
-const obj1 = {
-  abcdefghijklmnopqrstuvwxyz: 123
-};
+// const obj1 = {
+//   abcdefghijklmnopqrstuvwxyz: 123
+// };
 
-const abcdefghijklmnopqrstuvwxyz = obj1.abcdefghijklmnopqrstuvwxyz;
+// const {abcdefghijklmnopqrstuvwxyz} = obj1;
 
-console.log(abcdefghijklmnopqrstuvwxyz);
+// console.log(abcdefghijklmnopqrstuvwxyz);
 
 
 
+// let myPromise = new Promise((res,rej) =>{
+
+//   let number = Math.random();
+
+//   setTimeout(()=>{
+//     if (number > 0.5) {
+//       res('Number is more then 0.5');
+//     }
+//   },500)
+
+//   setTimeout(()=>{
+//     if (number <= 0.5) {
+//       rej('Number less or equal 0.5');
+//     }
+//   },500)
+// })
+
+// myPromise.then(data =>{
+//   console.log(data);
+// })
+
+// .catch(err =>{
+//   console.log(err)
+// })
+
+
+
+const resrej = () => {
+  let number = Math.random();
+  return new Promise((res, rej) => {
+    setTimeout(() => {
+      if (number > 0.5) {
+        res('Number is more then 0.5');
+      }
+    }, 500)
+
+    setTimeout(() => {
+      if (number <= 0.5) {
+        rej('Number less or equal 0.5');
+      }
+    }, 500)
+  });
+}
+
+const aaa = async () => {
+
+  try {
+    const rrr = await resrej();
+    console.log(rrr);
+  } catch (er) {
+    console.log(er)
+  }
+
+}
+
+aaa();
 
 
 
